7ee7f7e7af17f0b001b3dbd906651a658781dbbc
Matthew Martinez <matt.martinez2120@gmail.com>
2022-11-21 18:31:57 -0600

    added a solution.

--- src/Solutions.java
+++ src/Solutions.java
@@ -263,6 +263,19 @@ public class Solutions {
 
     }
     ///////////////////////////////////////////////////////////////////
+    public class Money {
+        public static int calculateYears(double principal, double interest,  double tax, double desired) {
+            int years = 0;
+            while (principal < desired) {
+                principal += (principal * interest) * (1 - tax);
+                years++;
+            }
+            return years;
+        }
+    }
+    ///////////////////////////////////////////////////////////////////
+
+    ///////////////////////////////////////////////////////////////////
 
     public static void main(String[] args) {
 

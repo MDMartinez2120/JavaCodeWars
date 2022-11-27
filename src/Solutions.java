9dbdcc51f30c698d8a19ae866e1973b64e9180aa
Matthew Martinez <matt.martinez2120@gmail.com>
2022-11-26 21:58:11 -0600

    added a solution.

--- src/Solutions.java
+++ src/Solutions.java
@@ -338,6 +338,23 @@ public class Solutions {
         }
     }
     ///////////////////////////////////////////////////////////////////
+    public static String getMiddle(String word) {
+        int position;
+        int length;
+
+        if (word.length() % 2 == 0)
+        {
+            position = word.length() / 2 - 1;
+            length = 2;
+        }
+        else
+        {
+            position = word.length() / 2;
+            length = 1;
+        }
+        return word.substring(position, position + length);
+    }
+    ///////////////////////////////////////////////////////////////////
 
     public static void main(String[] args) {
 

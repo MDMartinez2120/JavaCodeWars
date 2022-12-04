2fede9dec7d786ab26b8f13de3c3d8c1a851e5b8
Matthew Martinez <matt.martinez2120@gmail.com>
2022-12-03 23:33:11 -0600

    added a solution.

--- src/Solutions.java
+++ src/Solutions.java
@@ -405,7 +405,17 @@ public class Solutions {
         return Integer.bitCount(n);
     }
     ///////////////////////////////////////////////////////////////////
-
+    public static String accum(String s) {
+        StringBuilder bldr = new StringBuilder();
+        int i = 0;
+        for(char c : s.toCharArray()) {
+            if(i > 0) bldr.append('-');
+            bldr.append(Character.toUpperCase(c));
+            for(int j = 0; j < i; j++) bldr.append(Character.toLowerCase(c));
+            i++;
+        }
+        return bldr.toString();
+    }
     ///////////////////////////////////////////////////////////////////
 
     public static void main(String[] args) {
